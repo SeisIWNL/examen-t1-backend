ded23f92ac065e140e5ef6fe2865c57d3f4a8e92
Seis <ivanrup123@gmail.com>
2024-09-26 19:19:46 -0500

    Primera carga de datos - backend

---
+++ src/main/java/pe/edu/cibertec/VehiculoBack/VehiculoBackApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.VehiculoBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/VehiculoBack/VehiculoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.VehiculoBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
